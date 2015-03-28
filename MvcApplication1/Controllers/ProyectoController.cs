5a8d51fbf28f0770eec373ca2fa40d216ba9739b
hosscanoa <i201310358@cibertec.edu.pe>
2015-03-28 15:34:21 -0500

    Add Action and one view

---
+++ MvcApplication1/Controllers/ProyectoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        //
+        // GET: /Proyecto/
+
+        public ActionResult Index()
+        {
+            ViewBag.titulo = "Bienvenido";
+            ViewBag.detalle = "Desarrollo para Entorno Web";
+
+            return View();
+        }
+
+    }
+}
